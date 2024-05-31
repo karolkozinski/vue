370fe271f8aa5db612199480ec555783e997212e
Karol Koziński <larzuck@gmail.com>
2024-06-01 00:10:19 +0200

    modified:   app.js 	modified:   index.html

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             nazwa: 'pinassa',
             waga: 125
         } 
-}
+    },
+    methods: {
+        changeTitle () {
+            this.nazwa="galeon wielki az strasny"
+        }
+    }
 }) 
 
 app.mount('#app')
