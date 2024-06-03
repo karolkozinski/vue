dddf672d69cc05a3a227ce2fe5e059a7bca06837
Karol Koziński <larzuck@gmail.com>
2024-06-03 18:32:00 +0200

    V-for

--- app.js
+++ app.js
@@ -2,28 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             flaga: true,
-            nazwa: 'pinassa',
-            wypornosc: 40,
-            x: 0,
-            y: 0
+            ships: [
+                {nazwa: "Pinassa", wypornosc: "40", szybkosc: 14},
+                {nazwa: "Sloop", wypornosc: "60", szybkosc: 12},
+                {nazwa: "Barka", wypornosc: "80", szybkosc: 10}
+
+            ]
         } 
     },
     methods: {
         przestawFlage () {
             this.flaga=!this.flaga
             console.log(this.flaga)
-        },
-        handleEvent(e, data) {
-            console.log(e, e.type)
-            if (data) {
-                console.log(data)
-            }
-
-        },
-        handleMouseMove (e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
         }
+        
     }
 }) 
 
