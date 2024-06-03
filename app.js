6dd38b84600d3e6b553a7302e6be5e8d21097ec7
Karol Koziński <larzuck@gmail.com>
2024-06-03 19:40:33 +0200

    added images & atribute binding

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const app = Vue.createApp({
     data() {
         return {
             flaga: true,
+            url: "https://www.google.com/",
             ships: [
-                {nazwa: "Pinassa", wypornosc: "40", szybkosc: 14},
-                {nazwa: "Sloop", wypornosc: "60", szybkosc: 12},
-                {nazwa: "Barka", wypornosc: "80", szybkosc: 10}
+                {nazwa: "Pinassa", wypornosc: "40", szybkosc: 14, img: "img/Pinnace.webp"},
+                {nazwa: "Sloop", wypornosc: "60", szybkosc: 12, img: "img/Sloop.png"},
+                {nazwa: "Barka", wypornosc: "80", szybkosc: 10, img: "img/Barque.webp"}
 
             ]
         } 
